342fc2b775675c145e99971c631508dad5b72724
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-06-26 21:18:31 +0000

    edited code

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,69 +1,33 @@
 #!/usr/bin/env python3
+"""First-In First-Out caching module.
+"""
+from collections import OrderedDict
+
 from base_caching import BaseCaching
-from typing import Any
 
 
 class FIFOCache(BaseCaching):
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a FIFO
+    removal mechanism when the limit is reached.
     """
-    FIFOCache class inherits from BaseCaching and implements
-    a First-In-First-Out (FIFO) caching strategy.
-
-    Attributes:
-        Inherits MAX_ITEMS and cache_data from BaseCaching.
-
-    Methods:
-        put(key: str, item: Any) -> None:
-            Adds a key-value pair to the cache. If the cache
-            exceeds MAX_ITEMS, it removes the oldest item.
-        get(key: str) -> Any:
-            Retrieves the value associated with the given key from the cache.
-
-    Usage:
-        cache = FIFOCache()
-        cache.put('key1', 'value1')
-        value = cache.get('key1')
-        print(value)  # Output: 'value1'
-    """
-
     def __init__(self):
-        """
-        Initializes an instance of FIFOCache.
+        """Initializes the cache.
         """
         super().__init__()
+        self.cache_data = OrderedDict()
 
-    def put(self, key: str, item: Any) -> None:
-        """
-        Adds a key-value pair to the cache. If the cache exceeds MAX_ITEMS,
-        it removes the oldest item.
-
-        Args:
-            key (str): The key for the item to be cached.
-            item (Any): The item (value) to be cached.
-
-        Returns:
-            None
+    def put(self, key, item):
+        """Adds an item in the cache.
         """
         if key is None or item is None:
             return
-
-
-        if len(self.cache_data) + 1 > self.MAX_ITEMS:
-            oldest_key = next(iter(self.cache_data))
-            del self.cache_data[oldest_key]
-            print("DISCARD:", oldest_key)
         self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
 
-    def get(self, key: str) -> Any:
+    def get(self, key):
+        """Retrieves an item by key.
         """
-        Retrieves the value associated with the given key from the cache.
-
-        Args:
-            key (str): The key whose value is to be retrieved.
-
-        Returns:
-            Any: The value associated with the key if present, otherwise None.
-        """
-        if key is None:
-            return None
-
         return self.cache_data.get(key, None)
