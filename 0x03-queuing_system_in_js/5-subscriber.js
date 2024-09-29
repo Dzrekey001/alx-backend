cac95397989054081681d0c723299a64bf5b1cbf
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2024-09-29 18:45:10 +0000

    Initial Commit

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,13 +1,22 @@
 import redis from 'redis';
 
-const client = redis.createClient();
+const subscriber = redis.createClient();
 
-client.on('ready', () => {
+subscriber.on('ready', () => {
   console.log("Redis client connected to the server");
 });
 
-client.on('error', (error) => {
+subscriber.on('error', (error) => {
   console.log(`Redis client not connected to the server: ${error.message}`);
 });
 
+subscriber.subscribe('holberton school channel');
 
+subscriber.on('message', (channel, message) => {
+  if (message === 'KILL_SERVER') {
+    subscriber.unsubscribe('holberton school channel');
+    subscriber.quit();
+  } else {
+    console.log(`${message}`);
+  }
+})
